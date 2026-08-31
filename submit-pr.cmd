9aece18fae0632530b3f7c88312cf062184b0aac
amine <el.gorch@hotmail.fr>
2026-08-31 03:44:00 +0200

    fix: éviter le doublon WaitForMerge

--- submit-pr.cmd
+++ submit-pr.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\submit-pr.ps1" -WaitForMerge %*
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\submit-pr.ps1" %*
 exit /b %errorlevel%
